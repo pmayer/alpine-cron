f9186c3a586fb19a830ddbc8e98fb0dd874c7257
Philipp Mayer <philipp.mayer@paradoxon.at>
2022-03-28 12:40:36 +0200

    ~ pull before build

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,7 +2,7 @@ set -e
 
 DOCKER_REPO=paradoxon/alpine-cron
 DOCKER_TAG=$(curl --silent "https://download.docker.com/linux/static/stable/x86_64/" | sed -n 's/.*href="\(docker-[0-9][^"]*\).*/\1/p' | tail -n1 | sed -n 's/docker-\(.*\).tgz/\1/p')
-docker build . -t docker-cron
+docker build --pull -t docker-cron .
 docker tag $(docker images -q docker-cron) ${DOCKER_REPO}:${DOCKER_TAG}
 docker tag $(docker images -q docker-cron) ${DOCKER_REPO}:latest
 
