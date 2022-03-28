20c7b581c57acf68e59c660385b02bed86ade76e
Philipp Mayer <philipp.mayer@paradoxon.at>
2022-03-28 12:27:13 +0200

    ~ push explicitly only latest

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,3 +1,5 @@
+set -e
+
 DOCKER_REPO=paradoxon/alpine-cron
 DOCKER_TAG=$(curl --silent "https://download.docker.com/linux/static/stable/x86_64/" | sed -n 's/.*href="\(docker-[0-9][^"]*\).*/\1/p' | tail -n1 | sed -n 's/docker-\(.*\).tgz/\1/p')
 docker build . -t docker-cron
@@ -5,4 +7,5 @@ docker tag $(docker images -q docker-cron) ${DOCKER_REPO}:${DOCKER_TAG}
 docker tag $(docker images -q docker-cron) ${DOCKER_REPO}:latest
 
 docker login
-docker push ${DOCKER_REPO}
+docker push ${DOCKER_REPO}:${DOCKER_TAG}
+docker push ${DOCKER_REPO}:latest
