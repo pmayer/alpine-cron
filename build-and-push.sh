330d985d8d793a377cb2c627ac168b0293ef68f7
Philipp Mayer <philipp.mayer@paradoxon.at>
2022-12-21 17:25:40 +0100

    fix: use correct image

--- build-and-push.sh
+++ build-and-push.sh
@@ -6,8 +6,8 @@ docker build --pull -t docker-cron-sentry .
 
 DOCKER_TAG=$(docker run --rm -v /var/run/docker.sock:/var/run/docker.sock:ro docker-cron-sentry docker version --format '{{.Client.Version}}')
 
-docker tag $(docker images -q docker-cron) ${DOCKER_REPO}:${DOCKER_TAG}
-docker tag $(docker images -q docker-cron) ${DOCKER_REPO}:latest
+docker tag $(docker images -q docker-cron-sentry) ${DOCKER_REPO}:${DOCKER_TAG}
+docker tag $(docker images -q docker-cron-sentry) ${DOCKER_REPO}:latest
 
 docker login
 docker push ${DOCKER_REPO}:${DOCKER_TAG}
